c5e918b1a49907f9b67d9a6792fec3d6e74e72ca
pranavikadatla <datlapranavika205@gmail.com>
2023-12-03 19:59:04 +0530

    my last commit

--- script.js
+++ script.js
@@ -36,16 +36,17 @@ function getWeatherReport( lat,long){
         .catch(error => console.error(`Error fetching weather data: ${error.message}`));
     }
     function displayWeatherData(data){
-    
+         let time=secondsToTimeZoneString(data.timezone) ;
         weatherData.innerHTML=`
+        <h1>Your Weather Data</h1>
         <button class="weatherInfo">Location:${data.name}</button>
-        <button class="weatherInfo">Humidity:${data.main.humidity}</button>
-        <button class="weatherInfo">Time Zone:${data.timezone}</button ><br>
-        <button class="weatherInfo"> Pressure:${data.main.pressure}</button >
-        <button class="weatherInfo"> Wind Speed:${data.wind.speed}</button >
+        <button class="weatherInfo"> Wind Speed:${data.wind.speed}kmph</button >
+        <button class="weatherInfo">Humidity:${data.main.humidity}</button><br>
+        <button class="weatherInfo">Time Zone: GMT ${time.sign} ${time.hours}:${time.minutes}</button >
+        <button class="weatherInfo"> Pressure:${data.main.pressure}atm</button >
         <button class="weatherInfo">Wind Direction:${degreeToDirection(data.wind.deg)}</button><br>
         <button class="weatherInfo">Temperature:${data.main.temp}</button >
-        <button class="weatherInfo"> Feels Like:${data.main.feels_like}</button >`
+        <button class="weatherInfo"> Feels Like:${data.main.feels_like} degree</button >`
     }
     function degreeToDirection(degree){
         if (degree >= 337.5 || degree < 22.5) {
@@ -66,5 +67,13 @@ function getWeatherReport( lat,long){
             return "North West";
           }
     }
+    function secondsToTimeZoneString(seconds) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const remainingSeconds = seconds % 60;
+        const sign = hours >= 0 ? '+' : '-';
+        console.log({sign, hours: Math.abs(hours), minutes, seconds: remainingSeconds });
+        return {sign, hours: Math.abs(hours), minutes, seconds: remainingSeconds };
+      }
        
 
